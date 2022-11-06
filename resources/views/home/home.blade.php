f65072ff26d3d6ee2c0d9e19a5f9c44a93590854
mhan2000 <betitoas3@hotmail.com>
2022-11-05 20:14:07 -0600

    Librerias necesarias, modal y vistas prueba

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,5 @@
+@extends('base')
+@section('title', 'Inicio')
+@section('body')
+    <h1>Inicio</h1>
+@endsection
---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,5 @@
+@extends('base')
+@section('title', 'Inicia sesión')
+@section('body')
+    <h1>Prueba inicio sesion</h1>
+@endsection
